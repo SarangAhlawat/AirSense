7503288420bd8a29807302d3fd0a3326825a18f8
Sarang Ahlawat <sarangahlawat73@gmail.com>
2025-09-26 02:22:06 +0530

    .

--- src/components/common/Card.tsx
+++ src/components/common/Card.tsx
@@ -1,22 +1,14 @@
 import React from 'react'
 
-type CardProps<T extends keyof JSX.IntrinsicElements = 'div'> = {
-  as?: T
-  className?: string
+interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
   children?: React.ReactNode
-} & JSX.IntrinsicElements[T]
+}
 
-export default function Card<T extends keyof JSX.IntrinsicElements = 'div'>({ 
-  as, 
-  className = '', 
-  children, 
-  ...rest 
-}: CardProps<T>){
-  const Tag = (as || 'div') as any
+export default function Card({ className = '', children, ...rest }: CardProps){
   return (
-    <Tag {...rest} className={`card ${className}`.trim()}>
+    <div {...rest} className={`card ${className}`.trim()}>
       {children}
-    </Tag>
+    </div>
   )
 }
 
